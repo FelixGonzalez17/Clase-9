a9067b5eae4b9b0992904394f2ed5a05aee1e995
Felix Gonzalez <felix.gonzalez16@inacapmail.cl>
2025-05-21 16:38:31 -0400

    Clase 2

--- Proyecto-JavaScript-miercoles/script.js
+++ Proyecto-JavaScript-miercoles/script.js
@@ -1,32 +1,45 @@
- const students=[];
+const students=[];
 
- document.getElementById("studentForm").addEventListener("submit", function (e){
-    e.preventDefault();
+document.getElementById("studentForm").addEventListener("submit", function (e){
+   e.preventDefault();
 
-  const name=document.getElementById("name").value.trim();
-  const lastName=document.getElementById("lastName").value.trim();
-  const grade=parseFloat(document.getElementById("grade").value);
+ const name=document.getElementById("name").value.trim();
+ const lastName=document.getElementById("lastName").value.trim();
+ const grade=parseFloat(document.getElementById("grade").value);
 
-  if(!name || !lastName || isNaN(grade) || grade <1 || grade>7){
-    alert("Error al ingresar los datos")
-    return
-  }
-    
-  const student={name,lastName,grade}
-   students.push(student)
-   console.log(students)
-   addStudentToTable(student)
-
-   this.reset();
- });
-
- const tableBody=document.querySelector("#studentTable tbody");
- function addStudentToTable(student){
-   const row=document.createElement("tr");
-   row.innerHTML=`
-   <td>${student.name}</td>
-    <td>${student.lastName}</td>
-     <td>${student.grade}</td>
-   `;
-tableBody.appendChild(row)
+ if(!name || !lastName || isNaN(grade) || grade <1 || grade>7){
+   alert("Error al ingresar los datos")
+   return
  }
+   
+ const student={name,lastName,grade}
+  students.push(student)
+  console.log(students)
+  addStudentToTable(student)
+  calcularPromedio()
+
+  this.reset();
+});
+
+const tableBody=document.querySelector("#studentTable tbody");
+function addStudentToTable(student){
+  const row=document.createElement("tr");
+  row.innerHTML=`
+  <td>${student.name}</td>
+   <td>${student.lastName}</td>
+    <td>${student.grade}</td>
+  `;
+tableBody.appendChild(row)
+}
+
+const promedios= document.getElementById("average")
+
+function calcularPromedio(){
+    if(students.length===0){
+       averageDiv.textContent="Promedio General del Curso : N/A"
+       return
+     }
+     const total=students.reduce((sum,student)=>sum+student.grade,0);
+     const prom=total/students.length;
+     promedios.textContent="Promedio General del Curso : "+prom.toFixed(2);
+}
